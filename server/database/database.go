33130a0f593459db033b83cf7bca25f34a27cd76
Cesar <c.caycho46@gmail.com>
2021-05-27 18:15:51 -0500

    database fill user

--- server/database/database.go
+++ server/database/database.go
@@ -5,29 +5,7 @@ import (
 	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `bson:"username"`
-	Password string             `bson:"password"`
-	Role     string             `bson:"role"`
-	Deadline string             `bson:"deadline"`
-	Token    string             `bson:"token"`
-	Posts    []Post             `bson:"user_posts",omitempty`
-}
-
-type Post struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty"`
-	UserID  primitive.ObjectID `bson:"user_id"`
-	Content string             `bson:"content"`
-	Date    string             `bson:"date"`
-}
-
-type Token struct {
-	Content string `bson:"content"`
-}
 
 func CheckIfTokenIsInBlackList(token string) (check bool) {
 
--- server/database/open.go
+++ server/database/open.go
@@ -5,9 +5,31 @@ import (
 	"fmt"
 	"log"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Username string             `bson:"username"`
+	Password string             `bson:"password"`
+	Role     string             `bson:"role"`
+	Deadline string             `bson:"deadline"`
+	Token    string             `bson:"token"`
+	Posts    []Post             `bson:"user_posts",omitempty`
+}
+
+type Post struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	UserID  primitive.ObjectID `bson:"user_id"`
+	Content string             `bson:"content"`
+	Date    string             `bson:"date"`
+}
+
+type Token struct {
+	Content string `bson:"content"`
+}
 
 var (
 	Client                                                *mongo.Client
@@ -28,6 +50,40 @@ func init() {
 	PostsCollection = DB.Collection("posts")
 	BlackListCollection = DB.Collection("black_list")
 
+	users := []interface{}{
+		User{
+			ID:       [12]byte{},
+			Username: "cfabrica46",
+			Password: "01234",
+			Role:     "admin",
+			Deadline: "",
+			Token:    "",
+			Posts:    []Post{},
+		},
+		User{
+			ID:       [12]byte{},
+			Username: "arthuronavah",
+			Password: "12345",
+			Role:     "admin",
+			Deadline: "",
+			Token:    "",
+			Posts:    []Post{},
+		},
+		User{
+			ID:       [12]byte{},
+			Username: "luis",
+			Password: "lolsito123",
+			Role:     "member",
+			Deadline: "",
+			Token:    "",
+			Posts:    []Post{},
+		},
+	}
+
+	_, err = UsersCollection.InsertMany((context.TODO()), users)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func open() (client *mongo.Client, err error) {
